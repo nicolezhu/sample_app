ef55e73b45381f63fa8d2da6c0c7ef969847698b
nicolezhu <nzhu94@gmail.com>
2014-07-31 18:52:25 -0400

    finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session][:password])
 			# sign the user in and redirect to the user's show page
 			sign_in user
-			redirect_to user
+			redirect_back_or user
 		else
 			# create an error message and re-render the signin form
 			flash.now[:error] = 'Invalid email/password combination'
